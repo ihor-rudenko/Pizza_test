5243b4daa4fd9e206d7fea9b8513c4f8c3ca1cb6
Ihor Rudenko <i.rudenko@mittwald.de>
2025-08-17 17:18:03 +0200

    Added commennts for __inedex and posts file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,19 @@
+// Importiert die Funktion zum Erstellen einer Datei-basierten Route
+
 import { createFileRoute } from '@tanstack/react-router'
+
 // import { products } from "../gameiro.json"
 
+// Erstellt eine Route für die Startseite (URL: /)
+// Das '/' bedeutet: Diese Route wird angezeigt, wenn jemand die Hauptseite besucht
 export const Route = createFileRoute('/')({
+
+  // Legt fest, dass die Home-Komponente angezeigt werden soll
   component: Home,
 })
 
+// Definiert die Home-Komponente
+// Diese Komponente wird angezeigt, wenn jemand die Startseite besucht
 function Home() {
   return (
     <div className="p-2">
--- src/routes/posts.tsx
+++ src/routes/posts.tsx
@@ -1,16 +1,32 @@
+// Importiert drei wichtige Router-Funktionen:
+// Link: Für Navigation zwischen Seiten
+// Outlet: Für das Einbetten von Unterrouten
+// createFileRoute: Für das Erstellen der Route
 import { Link, Outlet, createFileRoute } from '@tanstack/react-router'
 
+// Erstellt eine Route für /posts (URL: example.com/posts)
+// Das '/posts' bedeutet: Diese Route wird angezeigt, wenn jemand auf example.com/posts klickt
 export const Route = createFileRoute('/posts')({
+  
+  // Ein "Loader" der Daten lädt, bevor die Seite angezeigt wird
+// Hier wird ein leeres Array zurückgegeben (keine echten Posts)
   loader: async () => [],
+  
+  // Legt fest, dass PostsComponent angezeigt werden soll
   component: PostsComponent,
 })
 
+// Definiert die Posts-Komponente
 function PostsComponent() {
+
+  // Holt die Daten aus dem Loader (hier: leeres Array)
   const posts = Route.useLoaderData()
 
+  // Gibt die Posts-Komponente zurück
   return (
     <div className="p-2 flex gap-2">
       Hallo World!!!
+
       <ul className="list-disc pl-4">
         {[...posts, { id: 'i-do-not-exist', title: 'Non-existent Post' }].map(
           (post) => {
@@ -23,6 +39,8 @@ function PostsComponent() {
         )}
       </ul>
       <hr />
+
+      {/* // Zeigt die Unterrouten an (z.B. /posts/123) */}
       <Outlet />
     </div>
   )
